302f17668ddda897a24b138993133a5558c0edae
Bumsss <allenbumanlag@gmail.com>
2023-04-04 00:08:37 +0800

    test

--- testing.js
+++ testing.js
@@ -18,4 +18,11 @@ window.addEventListener("scroll", reveal);
 
 setTimeout(function () {
   $(".loader_bg").fadeToggle();
-}, 4000);
+}, 1000);
+
+var content = document.getElementsByTagName("body")[0];
+var darkMode = document.getElementById("dark-change");
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  content.classList.toggle("night");
+});
