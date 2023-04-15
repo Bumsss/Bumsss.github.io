d829da3a2d3824848ba5bf9f356d51e693cb43d7
Bumsss <allenbumanlag@gmail.com>
2023-04-15 12:26:09 +0800

    asd

--- new/myjs.js
+++ new/myjs.js
@@ -1,3 +1,16 @@
 setTimeout(function () {
   $(".loader_bg").fadeToggle();
-}, 1000);
+}, 0000);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector("#hamburger");
+  const menus = document.querySelector("#menus");
+
+  hamburger.addEventListener("click", () => {
+    if (menus.className.includes("show")) {
+      menus.classList.remove("show");
+    } else {
+      menus.classList.add("show");
+    }
+  });
+});
