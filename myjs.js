bef0bec6d79b7e4cbf816ec90c65bb7a4d5aa09e
allenbumanlag <allenbumanlag@gmail.com>
2024-03-07 11:31:38 +0800

    loader

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
 setTimeout(function () {
   $(".loader_bg").fadeToggle();
-}, 10);
+}, 3050);
 
 window.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector("#hamburger");
